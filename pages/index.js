e962b7430ea345275755cc0453d22c0151db553a
jakhongirov <87989795+jakhongirov@users.noreply.github.com>
2022-01-02 18:31:15 +0500

    add courses and mentors section

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../src/components/layout/layout";
+import Courses from "../src/components/Courses/Courses";
+import Mentors from "../src/components/Mentors/Mentors";
 
 export default function Home({ post }) {
   return (
     <>
       <Layout>
-        <main>
-          <h1>Main</h1>
-        </main>
+        <Courses />
+        <Mentors />
       </Layout>
     </>
   );
